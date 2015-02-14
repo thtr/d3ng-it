8a7b6bc1e8290ca1408ff13397812a43a6e7b99c
Jim Montgomery <jimmont.com@gmail.com>
2015-02-13 17:04:37 -0800

    tinker with attribute changes

--- d3ngit.js
+++ d3ngit.js
@@ -58,25 +58,33 @@ angular.module('d3ngit', ['ngRoute'])
 	return {
 		restrict: 'E'
 		,scope: true
-		,template: '<form ng-submit="adjust()"><label for="w-{{id}}">width</label><input ng-model="svg.w" id="w-{{id}}" ng-blur="validWidth()"><input type=range min="{{minWidth}}" max="{{maxWidth}}" step="{{stepWidth}}" ng-model="svg.w"></form>'
+		,template: '<form ng-submit="adjust()"><label for="w-{{id}}">width {{svg.width.value}}</label><input type=range min="{{svg.width.min}}" max="{{svg.width.max}}" step="{{svg.width.step}}" ng-model="svg.width.value" ></form>'
 		,controller: function($scope){
 		// init the model
 			$scope.id = 'vis-'+$scope.$id;
 			$scope.model = d3.range(8, 876, 3);
-			$scope.minWidth = 200;
-			$scope.maxWidth = 700;
-			$scope.stepWidth = 10;
-			$scope.validWidth = function(){
-				var w = Number(this.w) || this.minWidth;
-				if(w < this.minWidth) w = this.minWidth;
-				else if(w > this.maxWidth) w = this.maxWidth;
 
-				w = w - (w % this.stepWidth);
+			$scope.valid = function(dimension){
+				var n = Number(dimension.value) || 0;
+				if(n < dimension.min) n = dimension.min;
+				else if(n > dimension.max) n = dimension.max;
+				else n -= (n % dimension.step);
 
-				this.w = w;
+				dimension.value = n;
 			};
-			$scope.adjust = function(){
-				this.validWidth();
+
+			$scope.validAdjustments = function(dimensions, previous){
+				var dimension, key, old, nu, changes = {length:0}, was;
+				for(key in dimensions){
+					old = ((previous||{})[key] || {}).value;
+					dimension = dimensions[key];
+					$scope.valid(dimension);
+					nu = dimension.value;
+					if(nu === old) continue;
+					changes.length++;
+					changes[key] = dimension;
+				};
+				return changes;
 			};
 		}
 		,link: function(scope, elem, attrs){
@@ -88,13 +96,13 @@ angular.module('d3ngit', ['ngRoute'])
 			};
 
 			scope.svg = {
-				w: 350
-				,h:300 
+				width: {value: 350, min: 200, max: 700, step: 10}
+				,height:{value: 300, min: 200, max: 400, step: 10}
 			};
 
 			// TODO improve so that values can be dynamically adjusted (via scope.$watch or attrs.$observe then set attribute to value)
 			var $svg = $compile(
-				$interpolate('<svg width="{{svg.w}}" height="{{svg.h}}" class="vis-sample" id="vis-sample-{{id}}" viewBox="0 0 {{svg.w}} {{svg.h}}" preserveAspectRatio="xMidYMid meet"></svg>')( scope )
+				$interpolate('<svg width="{{svg.width.value}}" height="{{svg.height.value}}" class="vis-sample" id="vis-sample-{{id}}" viewBox="0 0 {{svg.width.value}} {{svg.height.value}}" preserveAspectRatio="xMidYMid meet"></svg>')( scope )
 			)( scope );
 
 			// ? d3.select( svg[0] ).datum( scope.model );
@@ -102,14 +110,17 @@ angular.module('d3ngit', ['ngRoute'])
 			elem.append( $svg );
 
 			scope.$watch('svg', function(svg, old, scope){
-				// TODO update svg properties
-				console.log(+svg.w);
-				if(+svg.w === old.w) return;
-					$svg.attr('width', svg.w)
-					.attr(
-						'viewBox', $interpolate('0 0 {{svg.w}} {{svg.h}}')(scope)
-					);
+				var changes = scope.validAdjustments(svg);
+				if(!changes.length) return;
+
+				$svg
+					.attr('width', svg.width.value)
+					.attr('height', svg.height.value)
+					.attr('viewBox', '0 0 '+svg.width.value+' '+svg.height.value)
+
+				elem.find('input').prop('value',scope.svg.width.value);
 			}, true);
+
 			scope.$watchCollection('model', scope.render);
 		}
 	};
